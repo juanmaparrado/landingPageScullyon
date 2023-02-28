98f6d3eb79725da84b0ac99a7169ef5f79c338b5
juanmaparrado <juanmanuel.parrado.fp@iescampanillas.com>
2023-02-28 19:30:06 +0100

    add effects slideshow all okey

--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
-// Images for the slideshow
 const images = ["./recursos/slider1.jpg", "./recursos/slider2.jpg", "./recursos/slider3.jpg", "./recursos/slider4.jpg"];
-
-// Get the canvas element and its context
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-// Set the current image index to 0
 let currentImageIndex = 0;
 
-// Function to draw the current image on the canvas
+const filterEffects = [
+  "brightness(50%)",
+  "contrast(200%)",
+  "grayscale(75%)",
+  "blur(5px)",
+];
+const soundEffect = new Audio("../recursos/transition.mp3");
+
 function drawImage() {
-  // Load the image
   const image = new Image();
   image.src = images[currentImageIndex];
-
-  // When the image has loaded, draw it on the canvas
   image.onload = function() {
-    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-    // Schedule the next image to be drawn after 5 seconds
-    setTimeout(nextImage, 5000);
+  const filter = filterEffects[Math.floor(Math.random() * filterEffects.length)];
+  ctx.filter = filter;
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+  ctx.filter = "none";
+  soundEffect.play();
+  setTimeout(nextImage, 5000);
   };
 }
-
-// Function to go to the next image
 function nextImage() {
   currentImageIndex++;
   if (currentImageIndex >= images.length) {
@@ -32,5 +31,4 @@ function nextImage() {
   drawImage();
 }
 
-// Start the slideshow
 drawImage();
